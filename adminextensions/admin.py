17ddab82a8f2605420a53a9bbb3a2c04820ef346
Tim Heap <tim@ionata.com.au>
2012-10-04 10:02:41 +1000

    Add getter for object_tools
    Much like other getters for properties on ModelAdmin, this allows you to
    customise the returned object tools based on eg. the users permissions
    
    The use case for this one was a button that should only be present if
    the user has export permissions

--- adminextensions/admin.py
+++ adminextensions/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
+
 def extend(base, extra):
     if extra is None:
         return base
@@ -7,6 +8,7 @@ def extend(base, extra):
     base = base or {}
     base.update(extra)
     return base
+
 
 class ExtendedModelAdmin(admin.ModelAdmin):
 
@@ -20,24 +22,31 @@ class ExtendedModelAdmin(admin.ModelAdmin):
     change_form_template = 'adminextensions/change_form.html'
     change_list_template = 'adminextensions/change_list.html'
 
-
     valid_lookups = ()
 
+    def get_object_tools(self, request, method):
+        return self.object_tools.get(method, None)
 
     def add_view(self, request, form_url='', extra_context=None):
-        extra_context = extend(extra_context, {'object_tools': self.object_tools.get('add', None)})
-        return super(ExtendedModelAdmin, self).add_view(request, form_url, extra_context)
+        object_tools = self.get_object_tools(request, 'add')
+        extra_context = extend(extra_context, {'object_tools': object_tools})
+        return super(ExtendedModelAdmin, self).add_view(
+            request, form_url, extra_context)
 
     def change_view(self, request, object_id, form_url='', extra_context=None):
-        extra_context = extend(extra_context, {'object_tools': self.object_tools.get('change', None)})
-        return super(ExtendedModelAdmin, self).change_view(request, object_id, form_url, extra_context)
+        object_tools = self.get_object_tools(request, 'change')
+        extra_context = extend(extra_context, {'object_tools': object_tools})
+        return super(ExtendedModelAdmin, self).change_view(
+            request, object_id, form_url, extra_context)
 
     def changelist_view(self, request, extra_context=None):
-        extra_context = extend(extra_context, {'object_tools': self.object_tools.get('changelist', None)})
-        return super(ExtendedModelAdmin, self).changelist_view(request, extra_context)
-
+        object_tools = self.get_object_tools(request, 'changelist')
+        extra_context = extend(extra_context, {'object_tools': object_tools})
+        return super(ExtendedModelAdmin, self).changelist_view(
+            request, extra_context)
 
     def lookup_allowed(self, lookup, *args, **kwargs):
         if lookup.startswith(self.valid_lookups):
             return True
-        return super(ExtendedModelAdmin, self).lookup_allowed(lookup, *args, **kwargs)
+        return super(ExtendedModelAdmin, self).lookup_allowed(
+            lookup, *args, **kwargs)
