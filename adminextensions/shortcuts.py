5a44a9c7ae92b6577844294e376611ad7d62e779
Tim Heap <tim@ionata.com.au>
2012-12-18 14:36:55 +1100

    Tweak some shit

--- adminextensions/shortcuts.py
+++ adminextensions/shortcuts.py
@@ -3,6 +3,7 @@ from django.http import QueryDict
 from django.template.defaultfilters import truncatewords
 from django.utils.html import escape
 
+
 def model_search(text, model, args):
 
     app_label = model._meta.app_label
@@ -23,7 +24,8 @@ def model_search(text, model, args):
         return print_link(text, search_url, "model_search")
     return tool
 
-def model_link(text, model, pk_getter):
+
+def model_link(text, model, pk_getter, action="change"):
 
     app_label = model._meta.app_label
     module_name = model._meta.module_name
@@ -33,25 +35,31 @@ def model_link(text, model, pk_getter):
     def tool(context):
         pk = pk_getter(context['original'])
         if pk:
-            url = reverse(url_name, args=(pk,))
+            url = make_admin_url(model, pk=pk, action=action)
             return print_link(text, url, "model_search")
         else:
             return ''
     return tool
 
-def make_admin_url(obj, action="change"):
-    app_label = obj._meta.app_label.lower()
-    module_name = obj._meta.module_name
+
+def make_admin_url(model, pk=None, action="change"):
+    app_label = model._meta.app_label.lower()
+    module_name = model._meta.module_name
     url_name = "admin:%s_%s_%s" % (app_label, module_name, action)
-    url = reverse(url_name, args=(obj.pk,))
+
+    if pk is None:
+        pk = model.pk
+
+    url = reverse(url_name, args=(pk,))
     return url
 
-def link_field(field, action="change"):
+
+def link_field(field, action="change", formatter=unicode):
     def item(obj):
         related = getattr(obj, field)
 
-        url = make_admin_url(related, action)
-        name = unicode(related)
+        url = make_admin_url(related, action=action)
+        name = formatter(related)
 
         link = print_link(escape(name), url)
 
@@ -61,7 +69,9 @@ def link_field(field, action="change"):
     item.allow_tags = True
     return item
 
-def serialized_many_to_many_field(field, formatter=escape, joiner=', ', short_description=None, linked=False):
+
+def serialized_many_to_many_field(field, formatter=escape, joiner=', ',
+                                  short_description=None, linked=False):
     if short_description is None:
         short_description = field
 
@@ -75,10 +85,12 @@ def serialized_many_to_many_field(field, formatter=escape, joiner=', ', short_de
 
     return item
 
+
 def truncated_field(field, length=20):
     item = lambda obj: truncatewords(getattr(obj, field), length)
     item.short_description = field
     return item
 
-def print_link(text, url, className=""):
-    return u'<a href="%s" class="%s">%s</a>' % (url, className, text)
+
+def print_link(text, url, class_name=""):
+    return u'<a href="%s" class="%s">%s</a>' % (url, class_name, text)
