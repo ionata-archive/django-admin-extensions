4922d66d03b99d00658ff1f8b5844ef39ee57a36
Tim Heap <tim@ionata.com.au>
2012-12-18 15:35:09 +1100

    Version bump to v0.2.1

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ copyright = u'2012, Tim Heap'
 # built documents.
 #
 # The short X.Y version.
-version = "0.2.0"
+version = "0.2.1"
 # The full version, including alpha/beta/rc tags.
 release = version
 
